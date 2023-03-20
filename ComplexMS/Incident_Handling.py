16da030e9e832fc343563a6aa80592db203afc89
Angie <lachristie.2021@scis.smu.edu.sg>
2023-03-20 17:30:15 +0800

    added functions for sessions MS; added functions for incident handling Complex MS

--- ComplexMS/Incident_Handling.py
+++ ComplexMS/Incident_Handling.py
@@ -15,24 +15,25 @@ CORS(app)
 
 
 notification_URL = "http://localhost:5002/notification"
-penalty_URL = "http://localhost:5003/Penalty_Handling"
-session_URL = "http://localhost:5004/session-time"
+penalty_URL = "http://localhost:5200/Penalty_Handling"
+session_time_URL = "http://localhost:5003/session-time"
+close_session_URL = "http://localhost:5003/close-session"
 
 @app.route("/incident_handling", methods=['POST'])
 def incident_handling():
     # Simple check of input format and data of the request are JSON
     if request.is_json:
         try:
-            incident = request.get_json()
-            print("\nReceived an order in JSON:", incident)
+            session = request.get_json()
+            print("\nSession in JSON:", session)
 
             # do the actual work
             # 1. Send incident info
             
-            # result = processIncident(incident)
-            # print('\n------------------------')
-            # print('\nresult: ', result)
-            # return jsonify(result), result["code"]
+            result = processIncident(session)
+            print('\n------------------------')
+            print('\nresult: ', result)
+            return jsonify(result), result["code"]
 
         except Exception as e:
             # Unexpected error in code
@@ -52,16 +53,35 @@ def incident_handling():
         "message": "Invalid JSON input: " + str(request.get_data())
     }), 400
 
+def processIncident(session):
+    sessionId = session['id']
+    # 2. Update session closing time
+    # Invoke session microservice
+    print('\n-----Update session closing time (session microservice)-----')
+    closing_session_result = invoke_http(close_session_URL + sessionId, method='PUT', json=session)
+    print('closing_session_result:',closing_session_result)
+
+    # 3. Retrieve the session time
+    # Invoke session microservice
+    print('\n-----Retrieving session creation time (session microservice)-----')
+    session_time = invoke_http(session_time_URL + sessionId, method='GET', json=session)
+    print('session_time:',session_time)
 
 
-
-
+    # 7. Return confirmation of cancellation
+    return {
+        "code": 201,
+        "data": {
+            "cancellation": closing_session_result,
+            "cancelation_status": "confirmed"
+        }
+    }
 
 
 
 # Execute this program if it is run as a main script (not by 'import')
 if __name__ == "__main__":
-    print("This is flask " + os.path.basename(__file__) + " for placing an order...")
+    print("This is flask " + os.path.basename(__file__) + " for handling an incident...")
     app.run(host="0.0.0.0", port=5100, debug=True)
     # Notes for the parameters: 
     # - debug=True will reload the program automatically if a change is detected;

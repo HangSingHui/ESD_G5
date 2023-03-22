82af761e5c6a4cb94411c7b467bef1af6578c448
hangsinghui <shhang.2021@scis.smu.edu.sg>
2023-03-22 14:04:48 +0800

    notification ms more or less done

--- SimpleMS/Notification.py
+++ SimpleMS/Notification.py
@@ -21,7 +21,7 @@ app.config.update(mail_settings)
 mail = Mail(app)
 
 
-monitorBindingKey='*.notification'
+monitorBindingKey='#.notification'
 
 def receiveNotif():
     amqp_setup.check_setup()
@@ -34,21 +34,55 @@ def receiveNotif():
 #Actions on the message - send to the GMAIl API
 def callback(channel, method, properties, body): 
     print("\nReceived notification by " + __file__)
-    processNotif(json.loads(body))
+    processNotif(json.loads(body),method.routing_key)
     print() # print a new line feed
 
-def sendNotif(notif):
-    # print("Recording an order log:")
-    # print(order)
-    pass
+mail_signature = "\n Do contact us via our support email at inquiries.petsrus@gmail.com for any queries. \n Thank you for using Pets R Us! \n\n Best Regards, Pet R Us (With Pets, For Pets)"
 
-if __name__ == '__main__':
+def processNotif(notif,routing_key):
+    # print("Recording an order log:")
+    # print(order)  
+
+    #structure of AMQP message (JSON - routing key = accept.sitter.notification
+    # {
+    #    "sitterEmail": "lebubbub@gmail.com",
+    #    "jobTitle": "Dog Walking",
+    #     "sitterName": "Sally",
+    #       "jobID": 123   
+    # }
+
+    #Step 1: Check the routing key of the message
+    if routing_key == "accept.sitter.notification":
+        subject = "[Job Offered: ]" + str(notif.jobID)
+        body= "Dear " + notif.sitterName + ",\n We are pleased to inform you that your application for the job titled: "  + notif.jobTitle + "(" + str(notif.jobID) + ") has been accepted by the owner. Kindly indicate your acceptance of the offer in the Pet's R Us mobile application within the next 12 hours." 
+        recipient = notif.sitterEmail
+    
+     #structure of AMQP message (JSON - routing key = hold.payment.notification)
+    # {
+    #    "ownerEmail": "lebubbub@gmail.com",
+    #    "jobTitle": "Dog Walking",
+    #     "sitterName": "Sally",
+    #      "jobD": 123.
+    #       "totalPayable": $40,
+    #       "cardInfo": 1314 #last 4 numbers
+    # }
+    
+    elif routing_key=="hold.payment.notification":
+        subject = "[On-hold Payment: ]" + str(notif.jobID)
+        body= "Dear " + notif.ownerName + ",\n Your accepted sitter" + notif.sitterName + " has confirmed the acceptance of your job posting titled " + notif.jobTitle + "(" + str(notif.jobID) + "). We have successfully placed a hold of " + notif.totalPayable + " on your card ending with " + str(notif.cardInfo) + "."    
+        recipient = notif.ownerEmail
+    
+    body += mail_signature
     with app.app_context():
-        msg = Message(subject="Hello",
-                      sender=app.config.get("MAIL_USERNAME"),
-                      recipients=["lebubbub@gmail.com"], # replace with your email for testing
-                      body="This is a test email I sent with Gmail and Python!")
+        msg = Message(subject=subject,
+                    sender=app.config.get("MAIL_USERNAME"),
+                    recipients=[recipient], 
+                    body=body)
         mail.send(msg)
+
+if __name__ == "__main__":  # execute this program only if it is run as a script (not by 'import')    
+    print("\nThis is " + os.path.basename(__file__), end='')
+    print(": monitoring routing key '{}' in exchange '{}' ...".format(monitorBindingKey, amqp_setup.exchangename))
 
 
 

b421c0d5c6b03c1e0de6dbccd88fdd952cfb6c3f
Christian Koh <ckoh.2021@scis.smu.edu.sg>
2023-04-04 01:33:50 +0800

    new page and new settings

--- accept_app.js
+++ accept_app.js
@@ -1,27 +1,18 @@
+localStorage.clear()
 const get_all_url = "http://localhost:5005/job";
-const application_get_url = "http://localhost:5008/application/job";
-const sitter_info_url = "http://localhost:5001/sitter"
-
-// Get all sitters
-
-const fetch_sitter = fetch(sitter_info_url)
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-})
-
 
 const response = fetch(get_all_url).then(response => response.json())
 .then(data => {
     var jobs = data["data"]
     // console.log(jobs);
+
     for (let index = 0; index < jobs.length; index++) {
         // console.log(jobs[index]);
         var curr_job = `job${index+1}`
 
         var job = jobs[index];
         var job_id = jobs[index]["_id"]["$oid"];
-        // console.log(job_id);
+        console.log(job_id);
 
         var job_title = job["Title"];
         var job_desc = job["Description"];
@@ -38,66 +29,76 @@ const response = fetch(get_all_url).then(response => response.json())
 
         // console.log(job_title, job_desc, image, start_format_date, start_format_time, end_format_date, end_format_time, status, hourly_rate);
 
-        var modal_str = 
+        var temp_card_str = 
         `
-        <!-- View Job Application Modals -->
-        <button class="btn btn-primary btn-sm" data-bs-target="#${curr_job}app" data-bs-toggle="modal" id="${curr_job}all">View Applications</button>
-        <div class="modal fade" id="${curr_job}app" tabindex="-1">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5">Applications</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        <div class="col-lg-4 col-md-6 my-2" id="${curr_job}">
+            <div class="card h-100">
+                <img src="${image}" class="card-img-top">
+                <div class="card-body">
+                <h5 class="card-title">${job_title}</h5>
+                <p class="card-text">${job_desc}</p>
+            </div>
+            <div class="card-footer d-flex justify-content-between">
+                <!-- View More Info Modal -->
+                <button type="button" class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#${curr_job}Modal" id="${curr_job}viewinfo">
+                    View More Info
+                </button>
+                <!-- Modal -->
+                <div class="modal fade" id="${curr_job}Modal" tabindex="-1">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="${curr_job}ModalLabel">${job_title}</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                            </div>
+                            <div class="modal-body" id="${curr_job}_info">
+                                <p>Job Description: ${job_desc}</p>
+                                <p>Starting Date & Time: ${start_format_date} ${start_format_time}</p>
+                                <p>Ending Date & Time: ${end_format_date} ${end_format_time}</p>
+                                <p>Hourly Rate: $${hourly_rate}/hr</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
                     </div>
-                    <div class="modal-body">
-                        <ul class="list-group list-group-flush">
+                </div>        
         `
 
-        modal_li_str = "";
-        modal_info_str = ""
+        if (status == "Matched") {
+            temp_card_str += 
+            `
+            <!-- Cancel Sitter -->
+            <button class="btn btn-danger btn-sm" onclick="cancel_sitter(this)" id="${curr_job}cancel">Cancel Sitter</button>
 
-        const app_response = fetch(`${application_get_url}/${job_id}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            applications = data.data;
-
-            for (let app_index = 0; app_index < applications.length; index++) {
-
-                app_ID = applications[app_index]["_id"]["$oid"];
-                temp_li = 
-                `
-                <li class="list-group-item d-flex justify-content-between align-items-center" id="${curr_job}app${app_index+1}" value="${app_ID}">
-                    Application ${app_index + 1}
-                    <button class="btn btn-primary" data-bs-target="#${curr_job}app${app_index +1}modal" data-bs-toggle="modal">View More</button>
-                </li>
-                
-                `
-                // model_li_str += temp_li;
-                
-            }
-
-            // modal_str += model_li_str;
-
+            <!-- View Job Application Modals -->
+            <button class="btn btn-primary btn-sm d-none" id="${curr_job}all" onclick="fetch_applications(${job_id})">View Applications</button>
+            `
+        }
+        else{
+            temp_card_str += 
+            `
+            <!-- Cancel Sitter -->
+            <button class="btn btn-danger btn-sm d-none" onclick="cancel_sitter(this)" id="${curr_job}cancel">Cancel Sitter</button>
             
-
-            
-
-
-        })
-
-        
-
-
-
-        if (status == "Open") {
-            
-            
-
-
+            <!-- View Job Application Modals -->
+            <button class="btn btn-primary btn-sm" id="${curr_job}all" onclick="fetch_applications('${job_id}')">View Applications</button>
+            `
         }
 
+        temp_card_str += 
+        `
+                </div>
+            </div>
+        </div>
+        `
+
+         document.getElementById("job_list").innerHTML += temp_card_str;
     }
+
+    // fetch_applications(job_id);
+
+    
 
 
     if (data.code === 404) {
@@ -111,30 +112,19 @@ const response = fetch(get_all_url).then(response => response.json())
     console.log(error);
 })
 
-// const get_all_job_applications = 
 
-// var test_application_id = '64293aea06864f6b8cac1f3a';
-// const accept_application_route = "http://localhost:5100/accept_app";
+function fetch_applications(job_id) {
 
-// fetch(`${accept_application_route}/${test_application_id}`)
-// .then(response => response.json())
-// .then(data => {
-//     console.log(response);
-//     if (data.code === 404) {
-//         console.log("Application not found");
-//     }
-//     else{
-//         console.log(data);
-//     }
-// })
-// .catch(error => {
-//     console.log(error);
-// })
+    console.log(job_id);
+    localStorage.setItem("job_id", job_id);
+        // localStorage.setItem("applications", applications);
+
+    window.location.href = "http://127.0.0.1:5500/view_applications.html"
+}
 
 
 
 
-window.sessionStorage
 let payment_made = sessionStorage.getItem('paid')
 
 if (payment_made != null) {
@@ -159,25 +149,25 @@ if (payment_made != null) {
     // console.log(sitter_info);
     document.getElementById(`${job_id}_info`).innerHTML += sitter_info
 
-    const pay_route = "http://localhost:5000/test";
+    // const pay_route = "http://localhost:5000/test";
             
-    let jsonData = JSON.stringify({
-        payment_made : "true"
-    })
+    // let jsonData = JSON.stringify({
+    //     payment_made : "true"
+    // })
 
-    fetch(pay_route, {
-        method : "POST",
-        headers:{
-            "Content-type":"application/json"
-        },
-        body: jsonData
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        result = data.data;
-        console.log(result);
-    })
+    // fetch(pay_route, {
+    //     method : "POST",
+    //     headers:{
+    //         "Content-type":"application/json"
+    //     },
+    //     body: jsonData
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log(data);
+    //     result = data.data;
+    //     console.log(result);
+    // })
 }
 
 function accept_application(application) {

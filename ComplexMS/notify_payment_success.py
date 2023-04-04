d3a19478278832288b519bab979cdb2f519e8c6e
hangsinghui <shhang.2021@scis.smu.edu.sg>
2023-04-05 01:39:09 +0800

    many updates

--- ComplexMS/notify_payment_success.py
+++ ComplexMS/notify_payment_success.py
@@ -20,37 +20,6 @@ payment_URL = "http://localhost:5006"
 session_URL = "http://localhost:5004/session"
 owner_URL = "http://localhost:5000/owner"
 
-# monitorBindingKey='#.payment'
-
-#Consume from AMQP first to get the payment details
-#Data sent from accept_app.py
-##########################################
-# _id
-# OwnerID 
-# Created
-# PetID 
-# SitterID (When a Pet Sitter is hired)
-# Title
-# Description
-# Status (Open - No Pet Sitter hired, Matched - Pet Sitter hired, Closed - Job Closed, Completed - Session linked to this job is completed)
-# Start_datetime
-# End_datetime
-# Payout (Amount to be paid to Pet Sitter)
-#Wait_list
-##########################################
-
-# def paymentNotif():
-#     amqp_setup.check_setup() 
-#     queue_name = "payment_success"
-#     amqp_setup.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-#     amqp_setup.channel.start_consuming() 
-
-# #Actions on the message - send to the Payment MS
-# def callback(channel, method, properties, body): 
-#     print("\nReceived notification by " + __file__)
-#     checkSuccess(json.loads(body),method.routing_key) 
-#     print() # print a new line feed
-
 #Actions after receiving the AMQP to hold payment on Owner's Account by accept_app.py
 
 @app.route("/process_payment_success/<string:price_id>",method="GET")
@@ -87,7 +56,7 @@ def process_payment_success(price_id):
 
     ownerEmail = get_owner["data"][0]["Email"]
     #4. Send ownerEmail via AMQP to broker
-    
+
     amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="pmt.hold.success.notification", body=ownerEmail, properties=pika.BasicProperties(delivery_mode = 2))
 
 
